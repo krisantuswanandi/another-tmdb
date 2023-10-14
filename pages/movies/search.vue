0c51767172dbd0a87a325fe85895fc21d3c763be
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-10-14 10:42:16 +0700

    input focus

--- pages/movies/search.vue
+++ pages/movies/search.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { vFocus } from "@/utils/directives"
+
 const keyword = ref("")
 const results = ref<MovieTv[]>([])
 
@@ -24,7 +26,7 @@ watch(keyword, debouncedSearch)
     <div class="absolute w-10 h-full flex justify-center items-center">
       <div class="i-lucide-search text-xl" />
     </div>
-    <input v-model="keyword" placeholder="Search..." class="border border-black w-full pl-10 pr-2 py-1">
+    <input v-model="keyword" v-focus placeholder="Search..." class="border border-black w-full pl-10 pr-2 py-1">
   </div>
   <div class="mt-4">
     <MediaList v-if="results.length" :list="results" />
