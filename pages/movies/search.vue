206d100a9f4ad238fe56b1a6341e323a34b83324
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-10-14 11:54:57 +0700

    search query param

--- pages/movies/search.vue
+++ pages/movies/search.vue
@@ -1,24 +1,34 @@
 <script setup lang="ts">
 import { vFocus } from "@/utils/directives"
 
-const keyword = ref("")
+const route = useRoute()
+const router = useRouter()
+
+const keyword = ref((route.query.q || "").toString())
 const results = ref<MovieTv[]>([])
 
-async function search() {
-  if (!keyword.value) {
+async function fetch(keyword: string) {
+  if (!keyword) {
     results.value = []
     return
   }
 
-  const { data } = await useFetch<SearchList>(`/api/tmdb/search/multi?query=${keyword.value}`)
+  const { data } = await useFetch<SearchList>(`/api/tmdb/search/multi?query=${keyword}`)
   if (data.value) {
     results.value = data.value.results
   }
 }
 
+async function search() {
+  router.replace({ query: { q: keyword.value } })
+}
+
 const debouncedSearch = useDebounceFn(search, 500)
 
 watch(keyword, debouncedSearch)
+watch(() => route.query.q as string, fetch)
+
+await fetch(keyword.value)
 </script>
 
 <template>
